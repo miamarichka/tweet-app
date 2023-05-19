5bcf5758e4d0c6a708c8f689b99186cb0b8fb573
miamarichka <ilchyshyn.marichka@gmail.com>
2023-05-19 22:17:02 +0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename='/tweet-app'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
