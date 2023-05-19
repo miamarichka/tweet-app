cc59911b97875306d3632e9be812eea90c6b64cd
miamarichka <ilchyshyn.marichka@gmail.com>
2023-05-20 00:22:44 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
-export default defineConfig({
-  plugins: [react(), pluginRewriteAll()],
-  base: '/tweet-app/',
-  // resolve: {
-  //   alias: {
-  //     'react-dom': '@hot-loader/react-dom',
-  //   },
-  // },
+// export default defineConfig({
+//   plugins: [react(), pluginRewriteAll()],
+//   base: '/tweet-app/',
+  
+//   // resolve: {
+//   //   alias: {
+//   //     'react-dom': '@hot-loader/react-dom',
+//   //   },
+//   // },
+// })
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react(), pluginRewriteAll()],
+    base: '/tweet-app/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+
+  return config
 })
