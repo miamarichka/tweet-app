e401b8a85e66a231694e18eb393da2769b1684a6
miamarichka <ilchyshyn.marichka@gmail.com>
2023-05-19 17:44:29 +0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-/** @format */
+import React from 'react'
+import { createRoot } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
 
-import React from "react";
-import { createRoot } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/tweet-app'>
+    <BrowserRouter basename="/tweet-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
-);
+)
