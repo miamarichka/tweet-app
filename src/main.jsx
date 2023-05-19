79e3ec244774be1e4a0f38f165b2e7e14cc2925b
miamarichka <ilchyshyn.marichka@gmail.com>
2023-05-19 17:35:44 +0300

    config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
-import App from './App.jsx'
-import './index.css'
+/** @format */
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import React from "react";
+import { createRoot } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename='/tweet-app'>
       <App />
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tweet-app/"
+  base: '/tweet-app/',
+  resolve: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+  },
 })
