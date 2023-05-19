262cbc7d2822799c2d8651b7197abdd97559117b
miamarichka <ilchyshyn.marichka@gmail.com>
2023-05-19 22:12:10 +0300

    rewriteplugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), pluginRewriteAll()],
   base: '/tweet-app/',
   resolve: {
     alias: {
