615482f1c4b40b338d6d3076cce6b65ed79398cf
miamarichka <miamarichka@users.noreply.github.com>
2023-05-19 19:18:04 +0000

    deploy: 5bcf5758e4d0c6a708c8f689b99186cb0b8fb573

--- assets/HomePage-63cb4041.js
+++ assets/HomePage-63cb4041.js
@@ -1,4 +1,4 @@
-import{i as t,j as e}from"./index-d000d5b6.js";const o=t.h1`
+import{i as t,j as e}from"./index-c17fd88a.js";const o=t.h1`
   color: black;
   text-align: center;
 `,c=()=>e.jsx("div",{children:e.jsx(o,{children:"Welcome to Home Page"})});export{c as default};
